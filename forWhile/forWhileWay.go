316c1beefb1d68bf6d83dd515eea0c6ba445e790
Anıl Aydın <anil_aydinn@outlook.com>
2020-07-28 20:17:06 +0300

    forWhileWay.go added.

---
+++ forWhile/forWhileWay.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	//for (while yöntemi)
+
+	sum := 1
+	for sum < 10 {
+		sum += sum
+		fmt.Println(sum)
+		time.Sleep(300 * time.Millisecond)
+	}
+}
