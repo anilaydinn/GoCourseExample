07a38df947b2a4d8e550e6ff843d6ccae8986b75
Anıl Aydın <anil_aydinn@outlook.com>
2020-07-28 09:53:07 +0300

    Initial commit

---
+++ naming/naming.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+}
+
+var name string = "golang"
+var Version string = "1.14.4" // dışarıdan erişilebilir
+
+// Tek satır açıklama
+
+/*
+çoklu
+açıklama
+satırı
+*/
