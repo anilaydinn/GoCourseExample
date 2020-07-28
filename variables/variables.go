bfa0cd71dec9413ce5fdc5ff1805c4c5362f3efc
Anıl Aydın <anil_aydinn@outlook.com>
2020-07-28 10:20:22 +0300

    variables.go added.

---
+++ variables/variables.go
@@ -0,0 +1,43 @@
+package main
+
+const aciklama = "Go uygulamasi"
+const pi = 3.14
+
+var (
+	degisken1 = "Anıl"
+	degisken2 = "Aydın"
+)
+
+func main() {
+	/*
+		var message string
+		message = "Merhaba Go!"
+		var message string = "Merhaba!"
+		var message = "Merhaba Go!"
+		var a, b, c int = 3, 4, 5
+		var message = "Hello World!"
+		var a, b, c = 3, true, 4.5
+		var a int
+		var b int
+		var c float64
+		var d bool
+		var c int
+		var k, o string = "abc", "xyz"
+		var p = 42
+		var s, b = "xyz", true
+		u := 55
+		v, n := "abc", true
+		message := "Merhaba Go!"
+		a, b, c := 4, "You", false
+		a := "Metin"
+		b := 'M'
+		c := `Metin`
+		var myFloat32 float32 = 44.
+		myComplex := complex(3, 4)
+	*/
+
+	//var sayi = 4
+	//sayi := 4
+
+	//fmt.Println(sayi)
+}
