dae2437a53fe1b0f76ad77580fda89018f459643
Anıl Aydın <anil_aydinn@outlook.com>
2020-07-30 10:07:13 +0300

    packageExample added.

---
+++ packageExample/libraries/libraries.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"../utils"
+)
+
+func main() {
+
+	n1, l1 := utils.FullName("Zaphod", "Beeblebrox")
+	fmt.Printf("Fullname: %v, number of chars: %v\n\n", n1, l1)
+
+	n2, l2 := utils.FullNameNakedReturn("Arthur", "Dent")
+	fmt.Printf("Fullname: %v, number of chars: %v\n\n", n2, l2)
+}
